0134083d031ac3a9eca9b20b141803337d5f2f65
Chris Long <clong@cl0ng.com>
2020-05-20 15:15:38 -0700

    WSL2 and Emacs mode

--- MakeWindows10GreatAgain.ps1
+++ MakeWindows10GreatAgain.ps1
@@ -1,8 +1,10 @@
-# Check to see if Anniversary Update is installed
+Check to see if Anniversary Update is installed
 if ([System.Environment]::OSVersion.Version.Build -lt 14393) {
-  Write-Host "Anniversary Update is required and not installed. Exiting."
+  Write-Host "Build version 14393 or greater is required to continue. Exiting."
   Exit
 }
+
+$user = $(whoami).split('\')[1]
 
 # Import the registry keys
 Write-Host "Importing registry keys..."
@@ -18,13 +20,37 @@ c:\Windows\SysWOW64\OneDriveSetup.exe /uninstall
 # Disable SMBv1
 Set-SmbServerConfiguration -EnableSMB1Protocol $false -Confirm:$false
 
+# Add Emacs Edit Mode to Powershell Profile
+Set-Content $PsHome\Profile.ps1 'Set-PSReadlineOption -EditMode Emacs'
+
+# Download and install ShutUp10
+Write-Host "Downloading ShutUp10..."
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+$shutUp10DownloadUrl = "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe"
+$shutUp10RepoPath = "C:\Users\$user\Desktop\OOSU10.exe"
+Invoke-WebRequest -Uri "$shutUp10DownloadUrl" -OutFile $shutUp10RepoPath
+
 # Remove all pinned items from Start Menu
 # https://community.spiceworks.com/topic/post/7417573
 (New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items()| foreach { ($_).Verbs() | ?{$_.Name.Replace('&', '') -match 'From "Start" UnPin|Unpin from Start'} | %{$_.DoIt()}  }
 
 # Install Linux Subsystem
+# https://docs.microsoft.com/en-us/windows/wsl/install-win10
 Write-Host "Installing the Linux Subsystem..."
-Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Windows-Subsystem-Linux"
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+if ([System.Environment]::OSVersion.Version.Build -lt 19041) {
+  Write-Host "Build 19041 or greated required to upgrade to WSL2"
+  Exit
+} else {
+  Write-Host "Good news! This OS version supports WSL2. Enabling now..."
+  try {
+    dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+    wsl --set-default-version 2
+    Write-Host "WSLv2 Enabled!
+  } catch {
+    Write-Host "Something went wrong while updating to WSL2..."
+  }
+}
 
 # Disable WPAD 
 # https://docs.microsoft.com/en-us/security-updates/securitybulletins/2016/ms16-077#workarounds
