c289d74eaf65f31df963d21157327373a7d7f681
Chris Long <clong@cl0ng.com>
2020-06-05 16:33:18 -0700

    Update MakeWindows10GreatAgain.ps1

--- MakeWindows10GreatAgain.ps1
+++ MakeWindows10GreatAgain.ps1
@@ -7,32 +7,39 @@ if ([System.Environment]::OSVersion.Version.Build -lt 14393) {
 $user = $(whoami).split('\')[1]
 
 # Import the registry keys
-Write-Host "Importing registry keys..."
+Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Importing registry keys..."
 regedit /s MakeWindows10GreatAgain.reg
 
 # Install Powershell Help items
+Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Installing Powershell help..."
 Update-Help -Force -ErrorAction SilentlyContinue
 
 # Remove OneDrive from the System
+Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Removing OneDrive..."
 taskkill /f /im OneDrive.exe
 c:\Windows\SysWOW64\OneDriveSetup.exe /uninstall
 
 # Disable SMBv1
+Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Disabling SMBv1..."
 Set-SmbServerConfiguration -EnableSMB1Protocol $false -Confirm:$false
 
 # Add Emacs Edit Mode to Powershell Profile
+Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Updating Powershell Profile..."
 Set-Content $PsHome\Profile.ps1 'Set-PSReadlineOption -EditMode Emacs'
-
-# Download and install ShutUp10
-Write-Host "Downloading ShutUp10..."
-[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
-$shutUp10DownloadUrl = "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe"
-$shutUp10RepoPath = "C:\Users\$user\Desktop\OOSU10.exe"
-Invoke-WebRequest -Uri "$shutUp10DownloadUrl" -OutFile $shutUp10RepoPath
 
 # Remove all pinned items from Start Menu
 # https://community.spiceworks.com/topic/post/7417573
+Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Cleaning up start menu..."
 (New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items()| foreach { ($_).Verbs() | ?{$_.Name.Replace('&', '') -match 'From "Start" UnPin|Unpin from Start'} | %{$_.DoIt()}  }
+
+# Download and install ShutUp10
+Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Downloading ShutUp10..."
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+$shutUp10DownloadUrl = "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe"
+$shutUp10RepoPath = "$home\AppData\Local\Temp\OOSU10.exe"
+Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Installing ShutUp10..."
+Invoke-WebRequest -Uri "$shutUp10DownloadUrl" -OutFile $shutUp10RepoPath
+. $shutUp10RepoPath shutup10.cfg /quiet /force
 
 # Install Linux Subsystem
 # https://docs.microsoft.com/en-us/windows/wsl/install-win10
